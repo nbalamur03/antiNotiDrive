e6ebf6933e4d60ef7bcca139e7026b7131931439
nbalamur03 <nbalamur@asu.edu>
2023-06-28 21:27:19 +0200

    Initial Commit

---
+++ phoneNotifications/phoneNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  phoneNotificationsApp.swift
+//  phoneNotifications
+//
+//  Created by Neha Balamurugan on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct phoneNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
